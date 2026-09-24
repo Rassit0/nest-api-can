87d4d83769d467da111b2f68b3c10a44da25a06c
Rassit0 <ariel.rr32@gmail.com>
2026-09-23 23:41:18 -0400

    removedefaultValue to login

--- prisma/migrations/20260820132800_remove_courseseason_category/migration.sql
+++ prisma/migrations/20260820132800_remove_courseseason_category/migration.sql
@@ -1,5 +1,5 @@
 -- DropForeignKey
-ALTER TABLE "course_seasons" DROP CONSTRAINT "course_seasons_categoryId_fkey";
+ALTER TABLE "course_seasons" DROP CONSTRAINT "course_seasons_category_id_fkey";
 
 -- AlterTable
-ALTER TABLE "course_seasons" DROP COLUMN "categoryId";
+ALTER TABLE "course_seasons" DROP COLUMN "category_id";
